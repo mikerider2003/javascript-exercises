a36939b42f301f66dfd3d70e4389a4c907de9072
mikerider2003 <90722390+mikerider2003@users.noreply.github.com>
2023-07-10 19:30:26 +0200

    Forth exercise solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
 const removeFromArray = function() {
 
+    let result = [];
+    let remove = [];
+
+    for(let i = 1; i < arguments.length; i++) {
+        remove.push(arguments[i]);
+    }
+
+    for(let item of arguments[0]) {
+        if (!(remove.includes(item))) {
+            result.push(item);
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
