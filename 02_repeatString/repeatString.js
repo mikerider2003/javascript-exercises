481eca7ec2584629ea6fe55a76d8b4b4993b8c05
mikerider2003 <90722390+mikerider2003@users.noreply.github.com>
2023-07-10 18:12:04 +0200

    Second exercise solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, n) {
+    if (n < 0) {
+        return "ERROR";
+    }
 
+    let newstring = "";
+    for(let i=1; i <= n; i++) {
+        newstring += string;
+    }
+    return newstring;
 };
 
 // Do not edit below this line
