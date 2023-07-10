1a114c43011c250d3305400ae18f0018f6f23ca7
mikerider2003 <90722390+mikerider2003@users.noreply.github.com>
2023-07-10 20:09:55 +0200

    4th and 5th exercise solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+    if (number1 < 1 || number2 < 1) {
+        return "ERROR";
+    }
 
+    if (typeof(number1) != "number" || typeof(number2) != "number") {
+        return "ERROR";
+    }
+
+    let sum = 0;
+    if (number1 < number2) {
+        for(let i = number1; i <= number2; i++) {
+            sum += i
+        }
+    }
+    else {
+        for(let i = number2; i <= number1; i++) {
+            sum += i
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
