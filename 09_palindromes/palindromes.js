bb8e8ab23b84ad7cd7fd9b3d7fdc4dc8a1632560
mikerider2003 <90722390+mikerider2003@users.noreply.github.com>
2023-07-19 00:19:19 +0200

    9th exercise solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    var result;
+    const size = Math.floor(string.length/2)
+    
+    string = string.toLowerCase();
+    // string = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~() ]/g,"")
+    
+    const PUNCTUATION = ".,\/#!$%\^&\*;:{}=\-_`~() ";
+    for (let i = 0; i < PUNCTUATION.length; i++) {
+        string = string.replaceAll(PUNCTUATION[i], "")
+    };
 
+    for (let i = 0; i < size; i++) {
+        if(string[i] != string[string.length - i - 1]){
+            return false;
+        };
+    };
+
+    return true;
 };
 
 // Do not edit below this line
