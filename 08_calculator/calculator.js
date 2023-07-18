05b25fc04a473941fed0dc2ed669a1616fe69be2
mikerider2003 <90722390+mikerider2003@users.noreply.github.com>
2023-07-18 21:57:59 +0200

    8th exercise solved

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,51 @@
-const add = function() {
-	
+const add = function(numbe1, numbe2) {
+	return numbe1 + numbe2;
 };
 
-const subtract = function() {
-	
+const subtract = function(numbe1, numbe2) {
+	return numbe1 - numbe2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  var result = 0;
+
+  if (array.length === 0) {
+    return 0;
+  } 
+
+	array.forEach(element => {
+    result += element;
+  });
+
+  return result;
 };
 
 const multiply = function() {
+  var result = arguments[0];
 
+  for (let i = 1; i < arguments.length; i++) {
+    result *= arguments[i];
+  };
+
+  return result;
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+	return number ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(factorial) {
+  var result = arguments[0];
+
+	if (factorial === 0) {
+    return 1;
+  };
+
+  for (let i = factorial - 1; i > 0; i--) {
+    result *= i;
+  };
+
+  return result;
 };
 
 // Do not edit below this line
