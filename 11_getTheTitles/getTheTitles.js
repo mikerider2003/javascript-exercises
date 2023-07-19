9ece38c216c06926c3f518d8e41a64b1dea70d0a
mikerider2003 <90722390+mikerider2003@users.noreply.github.com>
2023-07-19 20:13:49 +0200

    11th exercise solved

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
+    var result = [];
 
+    for(const element of array){
+        result.push(element["title"]);
+    };
+
+    return result;
 };
 
 // Do not edit below this line
