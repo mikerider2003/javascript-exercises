d34c7ebaeb36ef9aaffc2e3a25057951b6a29130
mikerider2003 <90722390+mikerider2003@users.noreply.github.com>
2023-07-19 20:02:50 +0200

    10th exercise solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if(num <= 0){
+        return "OOPS";
+    };
 
+    const NUM_ARRAY = [1, 1];
+
+    for(let i = 0; i < num - 2; i++){
+        NUM_ARRAY.push(NUM_ARRAY[i] + NUM_ARRAY[i + 1]);
+    };
+
+    return NUM_ARRAY[NUM_ARRAY.length - 1];
 };
 
 // Do not edit below this line
