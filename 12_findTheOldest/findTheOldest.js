7fda76a43a8f7a8fbc71e18d36e13de8a6b85411
mikerider2003 <90722390+mikerider2003@users.noreply.github.com>
2023-07-19 20:45:11 +0200

    12th exercise solved

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    var oldestAge = 0;
+    var result;
 
+    for(const element of array){
+        if(!("yearOfDeath" in element)){
+            element["yearOfDeath"] = 2023;
+        };
+
+        if((element["yearOfDeath"] - element["yearOfBirth"]) > oldestAge){
+            oldestAge = (element["yearOfDeath"] - element["yearOfBirth"]);
+            result = element;
+        };
+    };
+
+    return result;
 };
 
 // Do not edit below this line
